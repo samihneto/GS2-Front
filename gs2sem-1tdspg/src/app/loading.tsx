7379c9cd930797ddd9033f87af4c5827a23f9ac5
Samir Neto <samihneto@gmail.com>
2024-11-20 12:36:50 -0300

    Adicionando template.tsx e loading.tsx com um Spinner laranja.
    Co-authored-by: Felipe_Fidelix <felipe-2833@users.noreply.github.com>
    Co-authored-by: 7ilo <DGMMX@users.noreply.github.com>

--- gs2sem-1tdspg/src/app/loading.tsx
+++ gs2sem-1tdspg/src/app/loading.tsx
@@ -1,7 +1,9 @@
+import Spinner from "@/components/Spinner/Spinner";
+
 export default function Loading() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+        <Spinner/>
+    </div>
+  )
 }
